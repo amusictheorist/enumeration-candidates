bff2c425ca6a148a67bf44df79cd6c83280a6542
amusictheorist <a_millaresthomson@outlook.com>
2024-11-10 08:52:51 -0500

    Implement candidate methods

--- filters.rb
+++ filters.rb
@@ -2,15 +2,28 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+  @candidates.find { |candidate|
+  candidate[:id] == id }
+end
+
+def experienced?(candidate)
+  years_experience = candidate[:years_experience]
+  years_experience >= 2
+end
+
+def qualified_candidates(candidates)
+  candidates.select do |candidate|
+    experienced?(candidate) &&
+    candidate[:github_points] >= 100 &&
+    (candidate[:languages].include? ('Ruby') ||
+    candidate[:languages].include? ('Python')) &&
+    candidate[:date_applied] >= (Date.today - 15) &&
+    candidate[:age] > 17
   end
-  
-  def experienced?(candidate)
-    # Your code Here
+end
+
+def ordered_by_qualifications(candidates)
+  candidates.sort_by do |candidate|
+    [-candidate[:years_of_experience], -candidate[:github_points]]
   end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
-  # More methods will go below
+end
